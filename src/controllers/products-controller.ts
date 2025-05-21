2678026665e1ab2c4e83be72c8564a557eb32200
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-05-21 16:55:30 -0300

    Criando middleware de tratamento de execuções

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
 
 class ProductsController {
     async index(request: Request, response: Response, next: NextFunction) {
         try{
-
+            
             return response.json({mesage: "ok!"})
         }catch(error){
             next(error);
