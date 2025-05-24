d21ead50c71bf55b59459db861571570866a1135
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-05-24 13:22:16 -0300

    Validando dados do produto
    foi abordado o processo de criação de uma rota para cadastro de produto no Insomnia, utilizando requisições HTTP do tipo POST.

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,15 +1,29 @@
 import { NextFunction, Request, Response } from "express";
-import { AppError } from "@/utils/AppError";
+import { z } from "zod";
 
 class ProductsController {
-    async index(request: Request, response: Response, next: NextFunction) {
-        try{
-            
-            return response.json({mesage: "ok!"})
-        }catch(error){
-            next(error);
-        }
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      return response.json({ mesage: "ok!" });
+    } catch (error) {
+      next(error);
     }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0),
+      });
+
+      const { name, price } = bodySchema.parse(request.body);
+
+      return response.status(201).json({ name, price });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
-export {ProductsController}
+export { ProductsController };
