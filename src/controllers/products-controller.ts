2c20d7327ba7d591ccb0ef334b2d22bfeb21f62e
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-05-26 13:44:51 -0300

    Atualizando produtos
    Foi abordado o processo de atualização de dados de um produto em uma aplicação. Foi demonstrado como criar uma requisição de atualização.

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 class ProductsController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
-      const { name } = request.query;  
+      const { name } = request.query;
 
       const products = await knex<productRepository>("products")
         .select()
@@ -30,6 +30,31 @@ class ProductsController {
       await knex<productRepository>("products").insert({ name, price });
 
       return response.status(201).json();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+      .string()
+      .transform((value) => Number(value))
+      .refine((value) => !isNaN(value), { message: "Id must be a number" })
+      .parse(request.params.id);
+
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0),
+      });
+
+      const { name, price } = bodySchema.parse(request.body);
+
+      await knex<productRepository>("products")
+        .update({ name, price, updated_at: knex.fn.now() })
+        .where({ id });
+
+      return response.json();
     } catch (error) {
       next(error);
     }
